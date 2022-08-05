182e964d2bea3006cee62aa2b95864343ca94088
A7MED-TAREK <33725218+A7MED-TAREK@users.noreply.github.com>
2022-08-05 20:43:31 +0200

    A.Tarek Repository

---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using EcommerseApplication.Models;
+
+namespace EcommerseApplication.Repository
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAll();
+        Product Get(int Id);
+        void Create(Product Product);
+        void Update(int Id, Product Product);
+        void Delete(int Id);
+    }
+}
