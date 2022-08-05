219a39b04517cbb4d9d6029d3f97460d0d6e31b6
amrafifi11111@outlook.sa <amrafifi11111@outlook.sa>
2022-08-05 23:57:28 +0200

    updateAmr

---
+++ Repository/IUserPayement.cs
@@ -0,0 +1,12 @@
+﻿using EcommerseApplication.Models;
+
+namespace EcommerseApplication.Repository
+{
+    public interface IUserPayement
+    {
+        public void AddUSerPayment(User_Payement newPaement);
+        public void DeleteUserPayment(int id);
+        public User_Payement GetUserPayment(int id);
+        public void updateUserPayement(int id,User_Payement newPaement);
+    }
+}
