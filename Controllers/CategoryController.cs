376dfa4366c2f1c80e77a327069266b0e9ca7062
abc <abc@example.com>
2022-08-13 00:07:51 +0200

    ahmedupdatedeletcategorysubcategory

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using EcommerseApplication.Models;
+﻿using EcommerseApplication.DTO;
+using EcommerseApplication.Models;
 using EcommerseApplication.Respository;
 using EcommerseApplication.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -12,10 +13,12 @@ namespace EcommerseApplication.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly IProductCategory productCategoryRespository;
+        private readonly ConsumerRespons Respons;
 
-        public CategoryController(IProductCategory productCategory)
+        public CategoryController(IProductCategory productCategory, ConsumerRespons _Response)
         {
             productCategoryRespository = productCategory;
+            Respons = _Response;
         }
        
         [HttpGet]
@@ -28,22 +31,93 @@ namespace EcommerseApplication.Controllers
         [Route("AddnewCategory")]
         public IActionResult AddnewCategory(ProductCategoryViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Product_Category newCat = new Product_Category();
+                    newCat.Name = model.Name;
+                    newCat.Description = model.Description;
+                    newCat.Name_Ar = model.Name_Ar;
+                    newCat.Description_Ar = model.Description_Ar;
+                    newCat.CreatedAt = DateTime.Now;
+                    productCategoryRespository.AddCategory(newCat);
+                    return Ok(new { Success = true, Message = "succeded", Data = "saved" });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { Success = false, Message = ex.Message, Data = "Dontsaved" });
+                }
+            }
+            return BadRequest(new
+            {
+                Success = false,
+                Message = String.Join("; ", ModelState.Values.SelectMany(n => n.Errors)
+                .Select(m => m.ErrorMessage)),
+                Data = ""
+            });
+        }
+        [HttpPut("updateCategory")]
+        public IActionResult UpdateCategory(int Id,ProductCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    Product_Category oldCat = productCategoryRespository.GetCategoryByID(Id);
+                    oldCat.Name = model.Name;
+                    oldCat.Description = model.Description;
+                    oldCat.Name_Ar = model.Name_Ar;
+                    oldCat.Description_Ar = model.Description_Ar;
+                    oldCat.UpdatedAt = DateTime.Now;
+                    productCategoryRespository.UpdateOldCategory(oldCat);
+                    return Ok(new { Success = true, Message = "succeded", Data = "Updated" });
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new { Success = false, Message = ex.Message, Data = "Dontupdated" });
+                }
+            }
+            return BadRequest(new
+            {
+                Success = false,
+                Message = String.Join("; ", ModelState.Values.SelectMany(n => n.Errors)
+                .Select(m => m.ErrorMessage)),
+                Data = model
+            });
+
+        }
+        [HttpDelete("DeleteCategory")]
+        public IActionResult DeleteCategory(int Id)
+        {
             try
             {
-                Product_Category newCat = new Product_Category();
-                newCat.Name = model.Name;
-                newCat.Description = model.Description;
-                newCat.Name_Ar = model.Name_Ar;
-                newCat.Description_Ar = model.Description_Ar;
-                newCat.CreatedAt = DateTime.Now;
-                productCategoryRespository.AddCategory(newCat);
+                int result = productCategoryRespository.DeleteACategory(Id);
+                if (result == 1)
+                {
+                    Respons.succcess = true;
+                    Respons.Message = "Product Category Deleted successfuly";
+                    Respons.Data = "";
+                    return Ok(Respons);
+                }
+                else
+                {
+                    Respons.succcess = false;
+                    Respons.Message = "not found this product Category";
+                    Respons.Data = "";
+                    return Ok(Respons);
+                    return BadRequest(Respons);
+                }
             }
-            catch (Exception ex) {
-                return Ok(new { Success = false, Message = "Failed", Data = "Dontsaved" });
+            catch (Exception ex)
+            {
+                Respons.Message = ex.InnerException.Message;
+                Respons.succcess = false;
+                Respons.Data = "";
+                return BadRequest(Respons);
             }
-            return Ok(new { Success = true, Message = "succeded", Data = "saved" });
         }
-
 
     }
 }
